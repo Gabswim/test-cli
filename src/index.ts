e079e3fa08c689606aa38d8e45ca1f3d1cbef967
Gabriel Legault <gabriel.legault@goto.com>
2024-02-08 00:45:55 -0500

    Add yargs example

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,27 @@
 #!/usr/bin/env npx ts-node --esm
+import yargs from "yargs/yargs";
+import { hideBin } from "yargs/helpers";
 
-import { faker } from "@faker-js/faker"
+import { faker } from "@faker-js/faker";
 
-console.log('Hello, world!1')
-console.log(`fake user: ${faker.person.firstName()} ${faker.person.lastName()}`)
+yargs(hideBin(process.argv))
+  .command(
+    "add <num1> <num2>",
+    "add two numbers",
+    (yargs) =>
+      yargs
+        .positional("num1", {
+          type: "number",
+          describe: "the first number",
+        })
+        .positional("num2", {
+          type: "number",
+          describe: "the second number",
+        }),
+    (argv) => console.log(argv.num1 + argv.num2)
+  )
+  .demandCommand()
+  .help()
+  .parse();
+
+
