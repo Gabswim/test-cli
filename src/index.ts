912b5c5a46d30b504a7141dbe8d489412e4f238d
Gabriel Legault <gabriel.legault@goto.com>
2024-02-08 00:07:55 -0500

    trying with a library

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
 #!/usr/bin/env npx ts-node --esm
 
+import { faker } from "@faker-js/faker"
+
 console.log('Hello, world!1')
+console.log(`hello ${process.argv[2]}!`)
+console.log(`fake user: ${faker.person.firstName()} ${faker.person.lastName()}`)
