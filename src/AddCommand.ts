2a8ceec4843ab19dee6f54553e8a5ee707799bcb
Gabriel Legault <gabriel.legault@goto.com>
2024-02-08 01:43:49 -0500

    Refactor out

---
+++ src/AddCommand.ts
@@ -0,0 +1,19 @@
+import { CommandModule } from "yargs";
+
+export const AddCommand = {
+  command: "add <num1> <num2>",
+  describe: "add two numbers",
+  builder: (yargs) =>
+    yargs
+      .positional("num1", {
+        type: "number",
+        describe: "the first number",
+      })
+      .positional("num2", {
+        type: "number",
+        describe: "the second number",
+      }),
+  handler: ({num1, num2}) => {
+    console.log(`{num1} + {num2} = ${num1 + num2}`);
+  },
+} satisfies CommandModule<{}, { num1: number; num2: number }>;
--- src/index.ts
+++ src/index.ts
@@ -2,27 +2,10 @@
 import yargs from "yargs/yargs";
 import { hideBin } from "yargs/helpers";
 
-import { faker } from "@faker-js/faker";
+import { AddCommand } from "./AddCommand";
 
 yargs(hideBin(process.argv))
-  .command(
-    "add <num1> <num2>",
-    "add two numbers",
-    (yargs) =>
-      yargs
-        .positional("num1", {
-          type: "number",
-          describe: "the first number",
-        })
-        .positional("num2", {
-          type: "number",
-          describe: "the second number",
-        }),
-    (argv) => {
-        console.log(argv.num1 + argv.num2)
-        console.log(faker.person.firstName())
-    }
-  )
+  .command(AddCommand)
   .demandCommand()
   .strict()
   .help()
